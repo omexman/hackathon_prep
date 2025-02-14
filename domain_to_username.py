eb95bdc85b8aa5f7dff6802185f2cc1a0902943f
hard333777 <kharaman.daniil@gmail.com>
2025-02-14 09:38:16 +0100

    domain and main

---
+++ domain_to_username.py
@@ -0,0 +1,7 @@
+def add_domain_to_username(username):
+    """ takes username as an argument and adds
+    a domain name symbol to the username and return the email
+    Example: if the username is user123@
+             the function should return user123@email.com
+    """
+    return username + 'gmail.com'
